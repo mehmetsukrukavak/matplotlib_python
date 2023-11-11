68a0a549c66bea76cf8a21735ef1f73aff0fb439
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-11 14:30:57 +0300

    fundamentals

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ age_list = np.array(random.sample(range(20,80),10))
 weight_list = np.array(random.sample(range(70,120),10))
 
 
-plt.plot(age_list, weight_list)
+plt.plot(age_list, weight_list, "r")
 plt.xlabel("Age")
 plt.ylabel("Weight")
 plt.title("Age - Weight Graph")
